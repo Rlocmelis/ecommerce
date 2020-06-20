ffe82c4c70699f93c9c615c0319d285b328de109
Rlocmelis <47271031+Rlocmelis@users.noreply.github.com>
2020-06-21 00:24:46 +0300

    Merge origin/master
    Conflicts:
    	README.md

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,7 +2,6 @@
 
 @section('admin_content')
         <div class="wrapper without_header_sidebar">
-            <!-- contnet wrapper -->
             <div class="content_wrapper">
                     <!-- page content -->
                     <div class="login_page center_container">
@@ -51,4 +50,28 @@
                     </div>
             </div><!--/ content wrapper -->
         </div><!--/ wrapper -->
+
+        
+        
+            <div class="d-flex align-items-center justify-content-center bg-sl-primary ht-100v">
+
+      <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white">
+        <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">E-SHOP <span class="tx-info tx-normal">admin</span></div>
+        <div class="tx-center mg-b-60">Lots of work to do</div>
+
+        <form>
+   
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Enter your username">
+        </div><!-- form-group -->
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Enter your password">
+          <a href="" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
+        </div><!-- form-group -->
+        <button type="submit" class="btn btn-info btn-block">Sign In</button>
+
+        </form>
+      </div><!-- login-wrapper -->
+    </div><!-- d-flex -->
+    
 @endsection
